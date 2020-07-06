ef07161d8a90356d84348e33f97367e70cc5d766
Brad <github@bnjf.id.au>
2020-07-06 09:09:19 +0000

    update test

--- test.sh
+++ test.sh
@@ -2,12 +2,26 @@
 
 set -eu
 
+sh=ksh
+
 #for f in corpus/progc; do
 #for f in corpus/obj; do
 for f in corpus/*; do
-  sha256sum $f
-  for b in 10 11 12; do
-    env COMPRESH_BITS=$b ksh ./compre.sh - < $f |
-      (uudecode -o - | gunzip -d | sha256sum)
-  done
+  sha256sum $f | {
+    read d f
+    for b in 10 11 12; do
+      echo -n "f=$f b=$b "
+      TIME="\t%E real,\t%U user,\t%S sys" time \
+        env COMPRESH_BITS=$b $sh ./compre.sh - < $f |
+        tee "f=$(basename $f)_b=$b.out" |
+        uudecode -o - |
+        gunzip -d |
+        sha256sum | {
+          read td tf
+          [ "$d" = "$td" ] || { echo "failed ($d vs $td)"; exit 1; }
+          rm "f=$(basename $f)_b=$b.out"
+        }
+      echo "ok"
+    done
+  }
 done
