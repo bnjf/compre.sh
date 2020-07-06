23bc072b12a04b4f1063f8569a5839b00103cb56
Brad <github@bnjf.id.au>
2020-07-06 09:09:00 +0000

    change uu to be stateful.  neaten padding.

--- compre.sh
+++ compre.sh
@@ -14,59 +14,80 @@ d="$(mktemp -d)" || exit 1
 trap 'rm -r "$d"' EXIT
 
 # uuencode stuff {{{
+
 typeset -a uu_table=(
  " " \! \" \# \$ %  \& \' \( \) \* +  \, -  .  /
   0  1  2  3  4  5  6  7  8  9  :  \; \< \= \> \?
   @  A  B  C  D  E  F  G  H  I  J  K  L  M  N  O
-  P  Q  R  S  T  U  V  W  X  Y  Z  \[ \\ \] \^ _
-)
-typeset -a uu_q
+  P  Q  R  S  T  U  V  W  X  Y  Z  \[ \\ \] \^ _)
 typeset uu_buf=""
-uu_begin() { echo "begin 644 ${1:?}.Z"; }
-uu_end() { echo '`'; echo 'end'; }
-uu_line() {
-  [[ ${#uu_buf} -eq 0 ]] ||
-    echo "${uu_table[${#uu_buf} / 4 * 3]}${uu_buf}"
+
+uu_begin() {
+  echo "begin 644 ${1:?}.Z"
 }
+
+# 0: add in>>2, emit.  save (in & 3) << 4 -- 2 bits pending
+#    000000 00
+# 1: add in>>4, emit.  save (in & 15) << 2 -- 4 bits pending, b_0 complete
+#    000000 001111 1111
+# 2: add in>>6, emit.  emit (in & 63) -- b_1 and b_2 complete
+#    000000 001111 111122 222222
+
+typeset -i uu_state=0
+typeset -i uu_carry=0
 uu_encode8() {
-  set -- "${uu_q[@]}" "$@"
-  while (( $# >= 3 || ($# > 0 && "${1:-0}" < 0) )); do
-    typeset -i b0="$1" b1="$2" b2="$3"
-
-    if (( b0 < 0 )); then
-      uu_line
-      return
-    elif (( b1 < 0 )); then
-      uu_line
-      echo "${uu_table[1
-        ]}${uu_table[b0 / 4
-        ]}${uu_table[((b0 * 16) % 64) + (b1 / 16)]}\`\`"
-      return
-    elif (( b2 < 0 )); then
-      uu_line
-      echo "${uu_table[2
-        ]}${uu_table[b0 / 4
-        ]}${uu_table[((b0 * 16) % 64) + (b1 / 16)
-        ]}${uu_table[((b1 % 16) * 4)]}\`"
-      return
-    fi
-
-    uu_buf+="${uu_table[b0 / 4]}"
-    uu_buf+="${uu_table[((b0 * 16) % 64) + (b1 / 16)]}"
-    uu_buf+="${uu_table[((b1 % 16) * 4) + (b2 / 64)]}"
-    uu_buf+="${uu_table[b2 % 64]}"
-
-    if [[ ${#uu_buf} -ge 60 ]]; then
-      echo "${uu_table[45]}${uu_buf:0:60}"
-      uu_buf="${uu_buf:60}"
-    fi
-
-    shift 3
+  while (( $# )); do
+    typeset -i in="${1:?}"
+    case $uu_state in
+    0)  uu_buf+="${uu_table[in >> 2]}"
+        : $(( uu_carry = (in & 3) << 4 )) ;;
+    1)  uu_buf+="${uu_table[uu_carry | (in >> 4)]}"
+        : $(( uu_carry = (in & 15) << 2 )) ;;
+    2)  uu_buf+="${uu_table[uu_carry | (in >> 6)]}"
+        uu_buf+="${uu_table[in & 63]}"
+    esac
+    : $(( uu_state = (uu_state + 1) % 3 ))
+    shift
   done
-
-  # save the rest
-  uu_q=("$@")
 }
+
+uu_line() {
+  typeset -i len="${1:-}"
+  [[ ${#uu_buf} -eq 0 ]] && return 1                        # empty buf
+  [[ $len -gt 0 && ${#uu_buf} -lt $len ]] && return 1       # unfulfillable
+  if (( len > 0 )); then
+    echo "${uu_table[len / 4 * 3]}${uu_buf:0:len}"
+    uu_buf="${uu_buf:len}"
+  else
+    len=${#uu_buf}
+    while (( ${#uu_buf} % 4 )); do uu_buf+='`'; done        # pad buf
+    echo "len=$len #uu_buf=${#uu_buf} uu_state=$uu_state z=$z" >&2 # XXX
+    typeset -i x='((len + 3) / 4 * 3) - ((3 - uu_state) % 3)'
+    echo "${uu_table[x]}${uu_buf}"
+    uu_buf=""
+  fi
+  return 0
+}
+
+uu_end() {
+  # full lines
+  while uu_line 60; do :; done
+
+  # commit carry, but preserve state
+  if (( uu_state != 0 )); then
+    typeset -i uu_state_save=$uu_state
+    uu_encode8 0
+    uu_state=$uu_state_save
+  fi
+
+  # final partial line
+  uu_line || true
+
+  # uuencode outro
+  echo '`'
+  echo 'end'
+}
+
 # }}}
 
 typeset -i COMPRESH_BITS="${COMPRESH_BITS:-12}"
@@ -118,18 +139,20 @@ while read addr line; do
       : $(( y >>= 8, z -= 8 ))
     done
   done
+
+  # unbuffer pending lines
+  while uu_line 60; do :; done
 done < <(od -bv)
 
 # finalize lz
 [[ -f e ]] || { echo "$0: i'm lost" >&2; exit 1; }
 : $(( y |= ($(<e) << z), z += w ))
-while (( z >= 0 )); do
+while (( z > 0 )); do
   uu_encode8 $(( y & 255 ))
   : $(( y >>= 8, z -= 8 ))
 done
 
 # finalize uu
-uu_encode8 -1 -1                # pad up for incomplete bytes
 uu_end
 
 # vim:set ts=2 sts=2 sw=2 et ai tw=72 fdm=marker:
