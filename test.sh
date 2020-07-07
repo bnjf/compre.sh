f98f86403301f78ec9161a6759cfd982def0c6f9
Brad <github@bnjf.id.au>
2020-07-07 16:54:23 +0000

    test 9 too with ncompress

--- test.sh
+++ test.sh
@@ -6,22 +6,27 @@ sh=ksh
 
 #for f in corpus/progc; do
 #for f in corpus/obj; do
+id=1
 for f in corpus/*; do
   sha256sum $f | {
     read d f
-    for b in 10 11 12; do
-      echo -n "f=$f b=$b "
-      TIME="\t%E real,\t%U user,\t%S sys" time \
-        env COMPRESH_BITS=$b $sh ./compre.sh - < $f |
+    # need patched ncompress to test 9
+    # https://github.com/vapier/ncompress/issues/5
+    for b in 9 10 11 12 13 14 15 16; do
+    #for b in 10 11 12; do
+      echo "testing f=$f b=$b "
+      #TIME="(%E real, %U user, %S sys)$(tput sc)" time \
+      env COMPRESH_BITS=$b $sh ./compre.sh - < $f |
+        pv -tab |
         tee "f=$(basename $f)_b=$b.out" |
         uudecode -o - |
-        gunzip -d |
+        ncompress -d |
         sha256sum | {
           read td tf
           [ "$d" = "$td" ] || { echo "failed ($d vs $td)"; exit 1; }
           rm "f=$(basename $f)_b=$b.out"
         }
-      echo "ok"
+      echo "\tOK!"
     done
   }
 done
